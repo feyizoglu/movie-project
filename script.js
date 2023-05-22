2d68e32f152a019e1f95ee84f57a886102b0a2f3
Gürkan <gurkanyigitgr@gmail.com>
2023-05-22 20:35:22 +0300

    add movie details and actor section

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
-'use strict';
+"use strict";
 
 const TMDB_BASE_URL = "https://api.themoviedb.org/3";
 const PROFILE_BASE_URL = "http://image.tmdb.org/t/p/w185";
 const BACKDROP_BASE_URL = "http://image.tmdb.org/t/p/w780";
+// const searchURL =
+//   TMDB_BASE_URL + "/search/movie?" + "6f4621a95489dc57656a3d8c6fd529a1";
 const CONTAINER = document.querySelector(".container");
+const moviesContainer = document.querySelector(".movies-container");
+const form = document.getElementById("form");
+const search = document.getElementById("search");
 
 // Don't touch this function please
 const autorun = async () => {
@@ -13,15 +18,14 @@ const autorun = async () => {
 
 // Don't touch this function please
 const constructUrl = (path) => {
-  return `${TMDB_BASE_URL}/${path}?api_key=${atob(
-    "NTQyMDAzOTE4NzY5ZGY1MDA4M2ExM2M0MTViYmM2MDI="
-  )}`;
+  return `${TMDB_BASE_URL}/${path}?api_key=${"6f4621a95489dc57656a3d8c6fd529a1"}`;
 };
 
 // You may need to add to this function, definitely don't delete it.
-const movieDetails = async (movie) => {
+const movieDetails = async (movie, actors) => {
   const movieRes = await fetchMovie(movie.id);
-  renderMovie(movieRes);
+  const actorRes = await fetchActors(actors);
+  renderMovie(movieRes, actorRes);
 };
 
 // This function is to fetch movies. You may need to add it or change some part in it in order to apply some of the features.
@@ -37,27 +41,35 @@ const fetchMovie = async (movieId) => {
   const res = await fetch(url);
   return res.json();
 };
+const fetchActors = async (movieId) => {
+  const url = constructUrl(`movie/${movieId}/credits`);
+  const res = await fetch(url);
+  const act = await res.json();
+  return act.cast;
+};
 
 // You'll need to play with this function in order to add features and enhance the style.
 const renderMovies = (movies) => {
-  movies.map((movie) => {
+  Object.values(movies).map((movie) => {
     const movieDiv = document.createElement("div");
+    movieDiv.classList.add("movies");
     movieDiv.innerHTML = `
         <img src="${BACKDROP_BASE_URL + movie.backdrop_path}" alt="${
       movie.title
     } poster">
         <h3>${movie.title}</h3>`;
     movieDiv.addEventListener("click", () => {
-      movieDetails(movie);
+      movieDetails(movie, movie.id);
+      console.log(movie.id);
     });
-    CONTAINER.appendChild(movieDiv);
+    moviesContainer.appendChild(movieDiv);
   });
 };
 
 // You'll need to play with this function in order to add features and enhance the style.
-const renderMovie = (movie) => {
+const renderMovie = (movie, actors) => {
   CONTAINER.innerHTML = `
-    <div class="row">
+    <div class="row text-white">
         <div class="col-md-4">
              <img id="movie-backdrop" src=${
                BACKDROP_BASE_URL + movie.backdrop_path
@@ -72,10 +84,27 @@ const renderMovie = (movie) => {
             <h3>Overview:</h3>
             <p id="movie-overview">${movie.overview}</p>
         </div>
-        </div>
-            <h3>Actors:</h3>
-            <ul id="actors" class="list-unstyled"></ul>
+        <div>
+            <h3 >Actors:</h3>
+            <ul id="actors" class="list-unstyled">
+            <li>${actors[0].name}</li>
+            <li>${actors[1].name}</li>
+            <li>${actors[2].name}</li>
+            <li>${actors[3].name}</li>
+            <li>${actors[4].name}</li>
+            </ul>
+            </div>
     </div>`;
 };
+
+// form.addEventListener("submit", (e) => {
+//   e.preventDefault();
+
+//   const searchTerm = search.value;
+
+//   if (searchTerm) {
+//     renderMovies(searchURL + "&query=" + searchTerm);
+//   }
+// });
 
 document.addEventListener("DOMContentLoaded", autorun);
